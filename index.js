45012825688e99a0196e8d3f6f962ebbd59aa33c
Jesse Greenough <jesse.greenough84@gmail.com>
2022-09-26 13:15:30 -0700

    Ceomplete as of now, futrure updates coming soon!

--- index.js
+++ index.js
@@ -3,3 +3,39 @@ const changeColor = () => {
     let x = Math.ceil(Math.random()*colors.length)
     document.getElementById('name').style.color = colors[x];
 }
+const openHoroscopes = () => {
+    console.log('here')
+    console.log(document.getElementById('horoscopesCarousel').style.visibility)
+
+    if (document.getElementById('horoscopesCarousel').style.visibility === 'hidden'){
+        console.log('what')
+        document.getElementById('horoscopesCarousel').style.visibility = 'visible';
+    } else {
+        document.getElementById('horoscopesCarousel').style.visibility = 'hidden'
+    }
+    return;
+}
+const openGreenExchange = () => {
+    console.log('here')
+    console.log(document.getElementById('greenExchangeCarousel').style.visibility)
+
+    if (document.getElementById('greenExchangeCarousel').style.visibility === 'hidden'){
+        console.log('what')
+        document.getElementById('greenExchangeCarousel').style.visibility = 'visible';
+    } else {
+        document.getElementById('greenExchangeCarousel').style.visibility = 'hidden'
+    }
+    return;
+}
+const openAdventureConnect = () => {
+    console.log('here')
+    console.log(document.getElementById('adventureConnectCarousel').style.visibility)
+
+    if (document.getElementById('adventureConnectCarousel').style.visibility === 'hidden'){
+        console.log('what')
+        document.getElementById('adventureConnectCarousel').style.visibility = 'visible';
+    } else {
+        document.getElementById('adventureConnectCarousel').style.visibility = 'hidden'
+    }
+    return;
+}
